84b466d686c778f55bb42fcbe7862d9dd890cb4f
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-10 21:11:55 +0600

    first commit

---
+++ squre by function.js
@@ -0,0 +1,15 @@
+function square (number){
+    console.log('given value',number)
+    borgo =number * number;
+    console.log('square of the given number is',borgo)
+}
+
+square(10)
+// console.log(borgo)
+
+
+function  hello (number){
+    const square = number * number;
+    console.log(square)
+}
+hello(10)
