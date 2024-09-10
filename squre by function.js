e06040c291120320fe9a11b86ec5442147722dbb
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-10 21:43:25 +0600

    summation by function added

--- squre by function.js
+++ squre by function.js
@@ -1,6 +1,19 @@
+// square calculate 
 function calculate (number){
     console.log('given value',number)
     const square = number * number;
     console.log('square of the given number is',square)
 }
  calculate(9)
+
+//  sum of two numbers 
+
+function numbers (num1 , num2){
+    console.log('Two numbers are ',num1, num2)
+
+    const sum = num1 + num2
+
+    console.log('The summation of two numbers is:',sum)
+}
+
+numbers ( 22 , 150)
